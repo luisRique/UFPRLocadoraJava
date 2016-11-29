f604ab6869846f94e431f0162d371e9f216a3248
deps11 <deps11@users.noreply.github.com>
2016-11-29 18:51:22 -0200

    Add files via upload

--- LocadoraAutomoveis/src/classes/Cliente.java
+++ LocadoraAutomoveis/src/classes/Cliente.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package classes;
 
-/**
- *
- * @author luis
- */
 public class Cliente {
+    public String nome;
+    public String sobrenome;
+    public String rg;
+    public String cpf;
+    public String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+    
         
 }
